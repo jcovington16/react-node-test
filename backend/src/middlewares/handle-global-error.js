c929a89edd8379ca1e8fc274292bf29affe613c4
TomDarcy2026 <tom.darcy2134@outlook.com>
2026-06-14 10:14:23 -0500

    Re-init

---
+++ backend/src/middlewares/handle-global-error.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const { ApiError, executeHandler } = require("../utils");
+
+const handleGlobalError = (err, req, res, next) => {
+    console.error(err);
+    if (err instanceof ApiError) {
+        return res.status(err.statusCode).json({ error: err.message });
+    }
+
+    return res.status(500).json({ error: "Internal server error" });
+}
+
+module.exports = { handleGlobalError };
